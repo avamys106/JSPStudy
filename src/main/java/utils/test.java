a06e3d22a0c5f4fd791d0950df9efd01a8bc19ce
avamys106 <avamys106@naver.com>
2024-02-22 18:10:32 +0900

    JSP test

---
+++ src/main/java/utils/test.java
@@ -0,0 +1,21 @@
+package utils;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		int a = 5655;
+		
+		for(int b=0 ; b<a/10 ; a++) {
+			b += a%10;
+			System.out.println(a%10);
+			System.out.println(a%10);
+			System.out.println(a%10);
+			System.out.println(a%10);
+			System.out.println(b);
+			return;
+		}
+		
+	}
+
+}
